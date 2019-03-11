8158c8dbfb022ac08d2455f0df69e1c8760448c4
eempc <evc484@gmail.com>
2019-03-11 19:57:53 +0000

    seed data success

---
+++ CarCompare/Models/SeedData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarCompare.Models {
+    public static class SeedData {
+        public static void Initialize(IServiceProvider serviceProvider) {
+            using (var context = new CarCompareContext(serviceProvider.GetRequiredService<DbContextOptions<CarCompareContext>>())) {
+                if (context.Car.Any()) return;
+
+                context.Car.AddRange(
+                    new Car {
+                        RegistrationMark = "AB12 BCD",
+                        Make = "Ford",
+                        Model = "Fiesta",
+                        TrimLevel = "LX",
+                        Mileage = 50000,
+                        Colour = "Red",
+                        Year = 2012,
+                        Price = 2000,
+                        Url = "Http://",
+                        Location = "London",
+                        DateAdded = DateTime.Now
+                    },
+                    new Car {
+                        RegistrationMark = "BC12 BCD",
+                        Make = "BMW",
+                        Model = "318i",
+                        TrimLevel = "SE",
+                        Mileage = 50000,
+                        Colour = "Red",
+                        Year = 2008,
+                        Price = 3000,
+                        Url = "Http://",
+                        Location = "York",
+                        DateAdded = DateTime.Now
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
--- CarCompare/Program.cs
+++ CarCompare/Program.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CarCompare.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CarCompare {
     public class Program {
         public static void Main(string[] args) {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            // See data
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+
+                try {
+                    var context = services.GetRequiredService<CarCompareContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception e) {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "Error in seed data");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
